3814546ceeb9a38c5d8264aa6112f70fe48ad8ee
Gagandeep <sandhugagan400@gmail.com>
2019-10-31 10:24:06 +1300

    Add project files.

---
+++ Food_Recipes/FoodIngredients.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace Food_Recipes
+{
+    public class FoodIngredients
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+        public string RecipeName { get; set; }
+        public string imageurl { get; set; }
+        public string ingredients { get; set; }
+        public string Recipeurl { get; set; }
+    }
+}
